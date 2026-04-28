7cb84421ee4f3d1bc3dc51b83549137c1f3ecbf2
Aaron Dower <20118118@setu.ie>
2026-04-28 15:43:45 +0100

    update

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -21,9 +21,17 @@ const playlistStore = {
     this.store.addItem(this.collection, id, this.array, song);
 },
 
-  addPlaylist(playlist) {
-    this.store.addCollection(this.collection, playlist);
-},
+      async addPlaylist(playlist, file, response) {
+    try {
+      playlist.picture = await this.store.addToCloudinary(file);
+      this.store.addCollection(this.collection, playlist);
+      response();
+    } catch (error) {
+      logger.error("Error processing playlist:", error);
+      response(error);
+    }
+  },
+
 
 editSong(id, songId, updatedSong) {
     this.store.editItem(this.collection, id, songId, this.array, updatedSong);
@@ -53,10 +61,21 @@ deleteSong(playlistId, songId) {
   }
 },
 
-removePlaylist(id) {
+  async removePlaylist(id, response) {
     const playlist = this.getPlaylist(id);
+
+    if (playlist.picture && playlist.picture.public_id) {
+      try {
+        await this.store.deleteFromCloudinary(playlist.picture.public_id);
+        logger.info("Cloudinary image deleted");
+      } catch (err) {
+        logger.error("Failed to delete Cloudinary image:", err);
+      }
+    }
+
     this.store.removeCollection(this.collection, playlist);
-},
+    response();
+  },
 
 };
 
